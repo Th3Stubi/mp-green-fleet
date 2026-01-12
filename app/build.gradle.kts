2819a40c351877fb4be4af1373eb2ff6b509faeb
Markus Rossmann <markus.rossmann@edu.fh-joanneum.at>
2026-01-12 20:03:43 +0100

    feat: create navbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.6")
 }
