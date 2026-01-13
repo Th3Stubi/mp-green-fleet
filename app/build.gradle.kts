402a37e46f0191a854e7fbaa54fa22068ff79c44
Markus Rossmann <markus.rossmann@edu.fh-joanneum.at>
2026-01-13 08:16:28 +0100

    chore: change dependencies to work for java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,5 +41,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.6")
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
 }
