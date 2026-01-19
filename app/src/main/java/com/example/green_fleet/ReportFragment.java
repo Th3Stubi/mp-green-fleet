30546a8a45de9f300a697f3db30ad3af20aed074
Markus Rossmann <markus.rossmann@edu.fh-joanneum.at>
2026-01-19 20:55:23 +0100

    feat: add date selection logic in report fragment

--- app/src/main/java/com/example/green_fleet/ReportFragment.java
+++ app/src/main/java/com/example/green_fleet/ReportFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RadioButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import android.widget.TextView;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.PopupMenu;
+
+import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +27,8 @@ public class ReportFragment extends Fragment {
     }
 
     private final List<RadioButton> radioButtons = new ArrayList<>();
+    private final List<TextView> filterLabels = new ArrayList<>();
+    private MaterialButton btnDate;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -38,7 +45,6 @@ public class ReportFragment extends Fragment {
             @Override
             public void onClick(View clickedView) {
                 RadioButton clickedBtn = (RadioButton) clickedView;
-
                 for (RadioButton rb : radioButtons) {
                     if (rb.getId() != clickedBtn.getId()) {
                         rb.setChecked(false);
@@ -50,5 +56,56 @@ public class ReportFragment extends Fragment {
         for (RadioButton rb : radioButtons) {
             rb.setOnClickListener(listener);
         }
+
+        // get all ids with filter in id
+        filterLabels.add(view.findViewById(R.id.tv_card_total_km_filter));
+        filterLabels.add(view.findViewById(R.id.tv_card_total_vehicles_filter));
+        filterLabels.add(view.findViewById(R.id.tv_card_total_gas_filter));
+        filterLabels.add(view.findViewById(R.id.tv_card_total_kwh_filter));
+        filterLabels.add(view.findViewById(R.id.tv_card_total_km_nv_filter));
+        filterLabels.add(view.findViewById(R.id.tv_card_total_co2_emissions_filter));
+        filterLabels.add(view.findViewById(R.id.tv_card_total_km_ev_filter));
+        filterLabels.add(view.findViewById(R.id.tv_card_total_co2_savings_filter));
+        btnDate = view.findViewById(R.id.mtbn_date);
+
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
+    }
+
+    private void showPopupMenu(View view) {
+        PopupMenu popup = new PopupMenu(requireContext(), view);
+
+        popup.getMenuInflater().inflate(R.menu.menu_date_selection, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.menu_today) {
+                    btnDate.setText("Today");
+
+                    for (TextView tv : filterLabels) {
+                        tv.setText("Today");
+                    }
+                    return true;
+
+                } else if (id == R.id.menu_month) {
+                    btnDate.setText("This month");
+
+                    for (TextView tv : filterLabels) {
+                        tv.setText("This month");
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        popup.show();
     }
 }
